89953eb236ad6247144e71656cc4a0afd5c9623b
Janne Pohjolainen <janne.pohjolainen@polarsquad.com>
2020-10-01 08:59:40 +0200

    Added all auth plugins and run formatting

--- main.go
+++ main.go
@@ -1,106 +1,107 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "time"
-  "math/rand"
+	"fmt"
+	"math/rand"
+	"os"
 	"path/filepath"
+	"time"
 
-  "golang.org/x/net/context"
+	"golang.org/x/net/context"
 
-  cli "github.com/urfave/cli/v2"
+	cli "github.com/urfave/cli/v2"
 
-  "k8s.io/client-go/tools/clientcmd"
-  "k8s.io/client-go/kubernetes"
-  "k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 var (
-  kubeconfig string
-  namespace string
+	kubeconfig string
+	namespace  string
 
-  cliFlags = []cli.Flag {
-    &cli.StringFlag{
-      Name: "kubeconfig",
-      Aliases: []string{"k"},
-      Value: filepath.Join(homedir.HomeDir(), ".kube", "config"),
-      Usage: "Path to kubeconfig",
-      EnvVars: []string{"KUBECONFIG"},
-      Destination: &kubeconfig,
-    },
-    &cli.StringFlag{
-      Name: "namespace",
-      Aliases: []string{"n"},
-      Value: "default",
-      Usage: "Namespace in K8s to deploy Socat",
-      Destination: &namespace,
-    },
-  }
+	cliFlags = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "kubeconfig",
+			Aliases:     []string{"k"},
+			Value:       filepath.Join(homedir.HomeDir(), ".kube", "config"),
+			Usage:       "Path to kubeconfig",
+			EnvVars:     []string{"KUBECONFIG"},
+			Destination: &kubeconfig,
+		},
+		&cli.StringFlag{
+			Name:        "namespace",
+			Aliases:     []string{"n"},
+			Value:       "default",
+			Usage:       "Namespace in K8s to deploy Socat",
+			Destination: &namespace,
+		},
+	}
 )
 
 func init() {
-  	rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
-  tunnel := k8sTunnel{}
+	tunnel := k8sTunnel{}
 
-  app := &cli.App{
-    Name: "k8stunnel",
-    Version: "1.0.1",
-    Compiled: time.Now(),
-    Usage: "Create tunnel through K8s",
-    UsageText: "k8stunnel [options] <host> <port> [localport]",
-    EnableBashCompletion: true,
-    Commands: []*cli.Command{},
-    Flags: cliFlags,
-    Action: func(c *cli.Context) error {
-      if c.NArg() < 2 {
-        fmt.Printf("You need to specify atleast 2 arguments.\n")
-        os.Exit(2)
-      }
-      
-      tunnel.destinationHost = c.Args().Get(0)
-      dPort := parseInt32OrExit(c.Args().Get(1))
-      tunnel.destinationPort = int32(dPort)
-      tunnel.containerPort = randomHighPort()
+	app := &cli.App{
+		Name:                 "k8stunnel",
+		Version:              "1.0.1",
+		Compiled:             time.Now(),
+		Usage:                "Create tunnel through K8s",
+		UsageText:            "k8stunnel [options] <host> <port> [localport]",
+		EnableBashCompletion: true,
+		Commands:             []*cli.Command{},
+		Flags:                cliFlags,
+		Action: func(c *cli.Context) error {
+			if c.NArg() < 2 {
+				fmt.Printf("You need to specify atleast 2 arguments.\n")
+				os.Exit(2)
+			}
 
-      if c.NArg() == 3 {
-        lPort := parseInt32OrExit(c.Args().Get(2))
-        tunnel.localPort = int32(lPort)
-      } else {
-        if dPort < 1000 {
-          tunnel.localPort = randomHighPort()
-        } else {
-          tunnel.localPort = tunnel.destinationPort
-        }
-      }
-      return nil
-    },
-  }
+			tunnel.destinationHost = c.Args().Get(0)
+			dPort := parseInt32OrExit(c.Args().Get(1))
+			tunnel.destinationPort = int32(dPort)
+			tunnel.containerPort = randomHighPort()
 
-  err := app.Run(os.Args)
-  if err != nil {
-    panic(err)
-  }
+			if c.NArg() == 3 {
+				lPort := parseInt32OrExit(c.Args().Get(2))
+				tunnel.localPort = int32(lPort)
+			} else {
+				if dPort < 1000 {
+					tunnel.localPort = randomHighPort()
+				} else {
+					tunnel.localPort = tunnel.destinationPort
+				}
+			}
+			return nil
+		},
+	}
 
-  if tunnel.destinationHost == "" {
-    os.Exit(2)
-  }
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
 
-  tunnel.ctx = context.Background()
+	if tunnel.destinationHost == "" {
+		os.Exit(2)
+	}
+
+	tunnel.ctx = context.Background()
 
 	tunnel.config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-  if err != nil {
+	if err != nil {
 		fmt.Printf("Error: %s\n", err)
-    os.Exit(2)
+		os.Exit(2)
 	}
 
 	if tunnel.client, err = kubernetes.NewForConfig(tunnel.config); err != nil {
 		panic(err)
 	}
 
-  tunnel.start()
+	tunnel.start()
 }
--- tunnel.go
+++ tunnel.go
@@ -1,125 +1,123 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "sync"
-  "time"
-  "strings"
-  "syscall"
-  "net/url"
-  "net/http"
-  "os/signal"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
 
-  "golang.org/x/net/context"
+	"golang.org/x/net/context"
 
-  apiv1 "k8s.io/api/core/v1"
-  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-  "k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 
-  "k8s.io/client-go/rest"
-  "k8s.io/client-go/informers"
-  "k8s.io/client-go/kubernetes"
-  "k8s.io/client-go/tools/cache"
-  "k8s.io/client-go/transport/spdy"
-  "k8s.io/client-go/tools/portforward"
-
-  // "k8s.io/apimachinery/pkg/util/wait"
-
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+	// "k8s.io/apimachinery/pkg/util/wait"
 )
 
 type k8sTunnel struct {
-  ctx context.Context
-  destinationHost string
-  destinationPort int32
-  containerPort int32
-  localPort int32
-  podName string
-  config *rest.Config
-  client *kubernetes.Clientset
-  stopCh chan struct{}
+	ctx             context.Context
+	destinationHost string
+	destinationPort int32
+	containerPort   int32
+	localPort       int32
+	podName         string
+	config          *rest.Config
+	client          *kubernetes.Clientset
+	stopCh          chan struct{}
 }
 
 var (
-  soCatPod = apiv1.Pod{
-    ObjectMeta: metav1.ObjectMeta{
-      Name: "",
-      Namespace: "",
-      Labels: map[string]string{
-        "app": "",
-      },
-    },
-    Spec: apiv1.PodSpec{
-      Containers: []apiv1.Container{
-        {
-          Name:  "socat",
-          Image: "alpine/socat:1.7.3.4-r0",
-          Command: []string{
-            "socat",
-          },
-          Args: []string{},
-          Ports: []apiv1.ContainerPort{
-            {
-              Name:          "tunnel",
-              Protocol:      apiv1.ProtocolTCP,
-              ContainerPort: 0,
-            },
-          },
-        },
-      },
-    },
-  }
+	soCatPod = apiv1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "",
+			Namespace: "",
+			Labels: map[string]string{
+				"app": "",
+			},
+		},
+		Spec: apiv1.PodSpec{
+			Containers: []apiv1.Container{
+				{
+					Name:  "socat",
+					Image: "alpine/socat:1.7.3.4-r0",
+					Command: []string{
+						"socat",
+					},
+					Args: []string{},
+					Ports: []apiv1.ContainerPort{
+						{
+							Name:          "tunnel",
+							Protocol:      apiv1.ProtocolTCP,
+							ContainerPort: 0,
+						},
+					},
+				},
+			},
+		},
+	}
 )
 
 func (tunnel *k8sTunnel) start() {
-  tunnel.deploy()
-  defer tunnel.delete()
+	tunnel.deploy()
+	defer tunnel.delete()
 
-  tunnel.startPortForward()
+	tunnel.startPortForward()
 }
 
 func (tunnel *k8sTunnel) deploy() {
-  suffix := randomChars(5)
-  tunnel.podName = fmt.Sprintf("k8stunnel-%s", suffix)
+	suffix := randomChars(5)
+	tunnel.podName = fmt.Sprintf("k8stunnel-%s", suffix)
 
-  soCatPod.ObjectMeta.Name = tunnel.podName
-  soCatPod.ObjectMeta.Namespace = namespace
-  soCatPod.ObjectMeta.Labels["app"] = tunnel.podName
-  soCatPod.Spec.Containers[0].Args = []string{
-    fmt.Sprintf("TCP-LISTEN:%d,fork", tunnel.containerPort),
-    fmt.Sprintf("TCP:%s:%d", tunnel.destinationHost, tunnel.destinationPort),
-  }
-  soCatPod.Spec.Containers[0].Ports[0].ContainerPort = tunnel.containerPort
+	soCatPod.ObjectMeta.Name = tunnel.podName
+	soCatPod.ObjectMeta.Namespace = namespace
+	soCatPod.ObjectMeta.Labels["app"] = tunnel.podName
+	soCatPod.Spec.Containers[0].Args = []string{
+		fmt.Sprintf("TCP-LISTEN:%d,fork", tunnel.containerPort),
+		fmt.Sprintf("TCP:%s:%d", tunnel.destinationHost, tunnel.destinationPort),
+	}
+	soCatPod.Spec.Containers[0].Ports[0].ContainerPort = tunnel.containerPort
 
-  podClient := tunnel.client.CoreV1().Pods(namespace)
+	podClient := tunnel.client.CoreV1().Pods(namespace)
 
-  // fmt.Printf("%#v\n", soCatPod)
+	// fmt.Printf("%#v\n", soCatPod)
 
-  fmt.Printf("Deploying '%s' with tunnel to '%s:%d'...", tunnel.podName,
-    tunnel.destinationHost, tunnel.destinationPort)
+	fmt.Printf("Deploying '%s' with tunnel to '%s:%d'...", tunnel.podName,
+		tunnel.destinationHost, tunnel.destinationPort)
 
-  _, err := podClient.Create(tunnel.ctx, &soCatPod, metav1.CreateOptions{})
-  if err != nil {
-    fmt.Printf("\nError: %v\n", err)
-    os.Exit(4)
-  }
-  // fmt.Printf("%#d\n", pod)
+	_, err := podClient.Create(tunnel.ctx, &soCatPod, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Printf("\nError: %v\n", err)
+		os.Exit(4)
+	}
+	// fmt.Printf("%#d\n", pod)
 
-  tunnel.watchPod("created")
+	tunnel.watchPod("created")
 }
 
 func (tunnel *k8sTunnel) delete() {
-  fmt.Printf("Deleting '%s'...", tunnel.podName)
-  podClient := tunnel.client.CoreV1().Pods(namespace)
-  deletePolicy := metav1.DeletePropagationForeground
-  err := podClient.Delete(tunnel.ctx, tunnel.podName, metav1.DeleteOptions{
-    PropagationPolicy: &deletePolicy,
-  })
-  if err != nil {
-    panic(err)
-  }
-  tunnel.watchPod("deleted")
+	fmt.Printf("Deleting '%s'...", tunnel.podName)
+	podClient := tunnel.client.CoreV1().Pods(namespace)
+	deletePolicy := metav1.DeletePropagationForeground
+	err := podClient.Delete(tunnel.ctx, tunnel.podName, metav1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	})
+	if err != nil {
+		panic(err)
+	}
+	tunnel.watchPod("deleted")
 }
 
 func (tunnel *k8sTunnel) startPortForward() {
@@ -145,66 +143,66 @@ func (tunnel *k8sTunnel) startPortForward() {
 	}()
 
 	go func() {
-    transport, upgrader, err := spdy.RoundTripperFor(tunnel.config)
-    if err != nil {
-      panic(err)
-    }
+		transport, upgrader, err := spdy.RoundTripperFor(tunnel.config)
+		if err != nil {
+			panic(err)
+		}
 
-    path := fmt.Sprintf("/api/v1/namespaces/%s/pods/%s/portforward",
-      namespace, tunnel.podName)
-    host := strings.TrimLeft(tunnel.config.Host, "https://")
-    dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport},
-      http.MethodPost, &url.URL{Scheme: "https", Path: path, Host: host})
+		path := fmt.Sprintf("/api/v1/namespaces/%s/pods/%s/portforward",
+			namespace, tunnel.podName)
+		host := strings.TrimLeft(tunnel.config.Host, "https://")
+		dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport},
+			http.MethodPost, &url.URL{Scheme: "https", Path: path, Host: host})
 
-    ports := fmt.Sprintf("%d:%d", tunnel.localPort, tunnel.containerPort)
-    fw, err := portforward.New(dialer, []string{ports}, stopCh, readyCh,
-      nil, stream.ErrOut)
-    if err != nil {
-      panic(err)
-    }
+		ports := fmt.Sprintf("%d:%d", tunnel.localPort, tunnel.containerPort)
+		fw, err := portforward.New(dialer, []string{ports}, stopCh, readyCh,
+			nil, stream.ErrOut)
+		if err != nil {
+			panic(err)
+		}
 
-    fw.ForwardPorts()
+		fw.ForwardPorts()
 	}()
 
-  select {
-    case <-readyCh:
-      fmt.Printf("\nReady to receive traffic to localhost:%d\n", tunnel.localPort)
-      fmt.Println("Press CTRL-C to quit..")
-      break
-  }
+	select {
+	case <-readyCh:
+		fmt.Printf("\nReady to receive traffic to localhost:%d\n", tunnel.localPort)
+		fmt.Println("Press CTRL-C to quit..")
+		break
+	}
 
-  wg.Wait()
+	wg.Wait()
 }
 
 func (tunnel *k8sTunnel) watchPod(w string) {
-  stopCh := make(chan struct{}, 1)
+	stopCh := make(chan struct{}, 1)
 
-  handlers := cache.ResourceEventHandlerFuncs{}
-  if w == "created" {
-    handlers = cache.ResourceEventHandlerFuncs{
-      AddFunc: func(obj interface{}) {
-        pod := obj.(*apiv1.Pod)
-        if pod.ObjectMeta.Name == tunnel.podName {
-          fmt.Println("done.")
-          close(stopCh)
-        }
-      },
-    }
-  } else {
-    handlers = cache.ResourceEventHandlerFuncs{
-      DeleteFunc: func(obj interface{}) {
-        pod := obj.(*apiv1.Pod)
-        if pod.ObjectMeta.Name == tunnel.podName {
-          fmt.Println("done.")
-          close(stopCh)
-        }
-      },
-    }
-  }
+	handlers := cache.ResourceEventHandlerFuncs{}
+	if w == "created" {
+		handlers = cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				pod := obj.(*apiv1.Pod)
+				if pod.ObjectMeta.Name == tunnel.podName {
+					fmt.Println("done.")
+					close(stopCh)
+				}
+			},
+		}
+	} else {
+		handlers = cache.ResourceEventHandlerFuncs{
+			DeleteFunc: func(obj interface{}) {
+				pod := obj.(*apiv1.Pod)
+				if pod.ObjectMeta.Name == tunnel.podName {
+					fmt.Println("done.")
+					close(stopCh)
+				}
+			},
+		}
+	}
 	resyncPeriod := 20 * time.Second
 	si := informers.NewSharedInformerFactory(tunnel.client, resyncPeriod)
 	si.Core().V1().Pods().Informer().AddEventHandler(handlers)
 	si.Start(stopCh)
 
-  <- stopCh
+	<-stopCh
 }
